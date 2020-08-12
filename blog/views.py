768c24c126de591009e08797b8ef0e034c6d5c1c
Rcmanaure <rcmanaure@gmail.com>
2020-08-12 16:56:49 -0400

    Templates. Se agregaron los archivos html y se le dieron estilo con bootstrap, se creo y paso un dummy data para ver en el context y se agrearon url absolutos.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'autor' : 'Ruben C',
+        'titulo' : 'Primer blog',
+        'contenido' : 'Primer post',
+        'date' : '12/08/2020',
+    },
+    {
+        'autor' : 'Gloria C',
+        'titulo' : 'segundo blog',
+        'contenido' : 'segundo post',
+        'date' : '12/08/2020',
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h2>About</h2>')
+    return render(request, 'blog/about.html', {'title':'About'})
