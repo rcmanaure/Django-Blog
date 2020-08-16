b02bfcd6b1f54d94b13ca84dbdcde7b065ed3567
Rcmanaure <rcmanaure@gmail.com>
2020-08-16 15:08:25 -0400

    Update User Profile. Se agreron formularios para actualizar la informacion del usuario(users/forms.py). Se modifico el metodo profile para aceptar los cambios(users/views.py). Se agrego el metodo save para modificar las imagenes(users/models.py). Y se hicieron los cambios en los templates para recibir y mostrar los cambios.

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,26 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
+# Formulario de registro costumizable.
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model =User
         fields = ['username', 'email', 'password1', 'password2']
+
+# Formulario para actualizar los datos del usuario.(Profile)
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model =User
+        fields = ['username', 'email']
+
+# Formulario para actualizar la foto del usuario(Profile)
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Libreria de Pillow para trabajar con imagenes.
+from PIL import Image
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,4 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # Metodo para ajustar imagenes automaticamente.
+    def save(self):
+        super().save()
+
+        # Declaramos la variable a trabajar.
+        img = Image.open(self.image.path)
+
+        # Se pone una condicional para poner el limite de altura y ancho de la imagen.
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
