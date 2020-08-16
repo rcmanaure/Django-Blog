a38e3c199ec8e3bb1c9d2d53171ffc68ca53b188
Rcmanaure <rcmanaure@gmail.com>
2020-08-15 21:15:59 -0400

    Se actualizo el comentario.

--- users/apps.py
+++ users/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
-
+    
+    # Para importar el signal que se creo en signals.py
     def ready(self):
         import users.signals
